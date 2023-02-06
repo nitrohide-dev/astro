08868ea74c38cd30ececf15a325fc4a0b13caa01
nitrohide-dev <66018901+nitrohide-dev@users.noreply.github.com>
2023-02-06 15:27:44 +0100

    Add files via upload

---
+++ public/public/sw.js
@@ -0,0 +1,19 @@
+// Service Worker code
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('v1').then(function (cache) {
+      return cache.addAll(['/']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
